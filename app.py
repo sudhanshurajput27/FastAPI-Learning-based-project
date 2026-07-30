68a36135e289e7ac0955a95662a3c45a3669a9ab
sudhanshurajput27 <sudhanshurajput.gd@gmail.com>
2026-07-30 15:00:35 +0530

    Https requests

--- app.py
+++ app.py
@@ -5,4 +5,25 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello FastAPI"}
+    return {"message": "Welcome"}
+
+
+@app.get("/about")
+def about():
+    return{"message": "This our first API"}
+
+
+@app.get("/students")
+def create_student():
+    return{
+        "message": "Student Created"
+    }
+
+
+@app.get("/search")
+def search_student(name: str, age: int):
+    return {
+        "name": name,
+        "age": age
+    }
+
