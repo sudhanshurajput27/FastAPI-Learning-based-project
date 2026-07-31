0c322e8b3f3f415f896804d586e8dd7490bd8d9b
sudhanshurajput27 <sudhanshurajput.gd@gmail.com>
2026-07-31 22:52:30 +0530

    Basic pydantic

--- app.py
+++ app.py
@@ -1,4 +1,58 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Student(BaseModel):
+    name: str
+    age:int
+
+@app.post("/students")
+def create_student(student: Student):
+    return student
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -26,4 +80,4 @@ def search_student(name: str, age: int):
         "name": name,
         "age": age
     }
-
+'''
