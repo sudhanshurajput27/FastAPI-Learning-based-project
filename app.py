b0507ebaa3b4461247ccfcfc7c527a59bc364809
sudhanshurajput27 <sudhanshurajput.gd@gmail.com>
2026-08-03 21:41:18 +0530

    Linked Our fast api project to actual model

--- app.py
+++ app.py
@@ -6,14 +6,22 @@ app = FastAPI()
 
 model = joblib.load("saved_model/model.pkl")
 
+
 class IrisData(BaseModel):
     feature1: float
     feature2: float
     feature3: float
     feature4: float
 
+
 @app.post("/predict")
 def predict(data: IrisData):
+
+    flower_names = {
+        0: "Setosa",
+        1: "Versicolor",
+        2: "Virginica"
+    }
 
     features = [[
         data.feature1,
@@ -24,76 +32,6 @@ def predict(data: IrisData):
 
     prediction = model.predict(features)
 
-    return {
-        "prediction": int(prediction[0])
-    }
+    result = flower_names[prediction[0]]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def home():
-    return {"message": "Welcome"}
-
-
-@app.get("/about")
-def about():
-    return{"message": "This our first API"}
-
-
-@app.get("/students")
-def create_student():
-    return{
-        "message": "Student Created"
-    }
-
-
-@app.get("/search")
-def search_student(name: str, age: int):
-    return {
-        "name": name,
-        "age": age
-    }
-'''
+    return {"prediction": result}
